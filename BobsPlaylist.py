a365498f704a3df628f5188d9e2b0752e6e27e69
Charlie Mulholland <charliemul04@gmail.com>
2023-01-03 12:23:58 -0500

    Initial Commit

---
+++ BobsPlaylist.py
@@ -0,0 +1,99 @@
+import re
+import time
+import os
+
+from spotipy import Spotify
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
+driver = webdriver.Chrome(options=chrome_options)
+
+bob_page_url = "https://1029bobfm.com"
+
+CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+CALLBACK_URI = os.getenv("SPOTIPY_CALLBACK_URI")
+PLAYLIST_URL = os.getenv("SPOTIPY_PLAYLIST_URL")
+
+# Sleep interval IN SECONDS between requests to the website
+sleep_interval = 120
+
+creds = SpotifyOAuth(client_id=CLIENT_ID,
+                     client_secret=CLIENT_SECRET,
+                     redirect_uri=CALLBACK_URI,
+                     scope="playlist-modify-public",
+                     open_browser=False)
+spotify = Spotify(auth_manager=creds)
+with open(".songs.tmp", "w") as f:
+    f.write("Processed URIs:")
+
+
+def check_song_exists(new_uri: str) -> bool:
+    with open(".songs.tmp", "r") as songs_f:
+        for uri in songs_f:
+            if (uri == new_uri):
+                return True
+    return False
+
+
+def get_song_info(driver: webdriver, page_url: str) -> (str, str):
+    driver.get(page_url)
+    song_info = driver.find_element(By.CLASS_NAME, "song").find_element(By.CLASS_NAME, "info")
+    artist = song_info.find_element(By.CLASS_NAME, "artist").text
+    title = song_info.find_element(By.CLASS_NAME, "title").text
+
+    artist = re.sub(r"[^a-zA-Z0-9 ]", "", artist)
+    title = re.sub(r"[^a-zA-Z0-9 ]", "", title)
+
+    return (title, artist)
+
+
+def spotify_search(api: Spotify, query_text: str) -> str:
+    search_result = api.search(query_text, limit=1)["tracks"]
+
+    if (int(search_result["total"]) == 0):
+        return None
+
+    return search_result["items"][0]["uri"]
+
+
+def add_to_playlist(api: Spotify, playlist_url: str, song_uri: str) -> None:
+    with open(".songs.tmp", "a") as songs_f:
+        songs_f.write(song_uri)
+
+    api.playlist_add_items(playlist_url, [song_uri])
+
+
+while True:
+    title, artist = get_song_info(driver, bob_page_url)
+
+    query_text = "{} artist:{}".format(title, artist)
+
+    print("Searching for {}".format(query_text))
+
+    search_result = spotify.search(query_text, limit=1)
+
+    uri = spotify_search(spotify, query_text)
+
+    if (uri is None):
+        print("Couldn't find song match, continuing")
+        continue
+
+    if (not check_song_exists(uri)):
+        print("Adding song to playlist")
+        add_to_playlist(spotify, PLAYLIST_URL, uri)
+    else:
+        print("Song already in playlist, skipping")
+
+
+    time.sleep(sleep_interval)
---
+++ README.md
@@ -0,0 +1,36 @@
+# Bob's Playlist
+## A Simple Python script to add songs played by 102.9 Bob FM in Southwest Florida to a Spotify Playlist
+
+### Setup
+1. All dependencies can be installed by running ```pip install requirements.txt```
+	* Or, alternatively, these can be installed manually through a different package manager
+2. If this is the first time using selenium, you will need to also install geckodriver for firefox.
+	* More instructions on this set are available [here](https://selenium-python.readthedocs.io/installation.html#drivers)
+	* Whichever geckodriver you download, needs to be placed in your environment's `PATH` (either `/usr/bin/` or `/usr/local/bin/` on UNIX systems)
+3. Environment Variables
+	* This script makes use of different environment variables for spotify access keys and playlist urls, namely:
+		* `SPOTIPY_CLIENT_ID`: client id key provided by spotify
+		* `SPOTIPY_CLIENT_SECRET`: client secret key provided by spotify
+		* `SPOTIPY_CALLBACK_URI`: callback url for spotify, this requires additional setup, see below
+		* `SPOTIPY_PLAYLIST_URL`: url for the spotify playlist for songs to be placed into, this needs to be a playlist created by the user signed into the API
+	* All of these can be set using the syntax: `export VARIABLE_NAME="variable value"`
+4. Spotify API Setup
+	* This program requires a spotify API account and a registered application.
+	* You can sign up for the api and make a new application [here](https://developer.spotify.com/dashboard/applications)
+	* Once you make a new application, export the application's client id and client secret to your environment
+	* Finally, set a callback URI for authentication.
+		* Two good ones to use are `http://localhost:8080` or `http://example.com/callback`.
+		* Whichever you choose will also need to be exported to the environment
+		* __NOTE:__ `http://localhost:8080` will not work if you are running this on a remote machine (i.e. a remote server) through ssh, use `http://example.com/callback` instead.
+
+
+### Usage:
+* Basic usage is extremely simple: `python BobsPlaylist.py`
+* You can also use `tmux` to continue this program and close the ssh session if running on a remote machine
+
+
+### Known Issues:
+* Authentication if finicky
+	* if you are getting the error `400 Client Error: Bad Request for url: https://accounts.spotify.com/api/token` try deleteing `.cache` in this directory.
+* This script keeps track of the songs it adds to the playlist and will not add duplicates __however__ it does not check through spotify if a song is already present so duplicates may be added that way. 
+	* Along the same lines: it may add duplicates of a song if they are different versions (remastered, live, radio edit, etc).
